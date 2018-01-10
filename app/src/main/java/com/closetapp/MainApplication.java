0ebe713232526bb9eac6f8870712e5f6a3848200
W540 <thanh.nguyen@seedcom.vn>
2018-01-10 10:35:22 +0700

    test

---
+++ app/src/main/java/com/closetapp/MainApplication.java
@@ -0,0 +1,44 @@
+package com.closetapp;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+import com.reactnativenavigation.NavigationApplication;
+import io.realm.react.RealmReactPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
+import com.rnfs.RNFSPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends NavigationApplication {
+  @Override
+  public boolean isDebug() {
+    // Make sure you are using BuildConfig from your own application
+    return BuildConfig.DEBUG;
+  }
+
+  protected List<ReactPackage> getPackages() {
+    // Add additional packages you require here
+    // No need to add RnnPackage and MainReactPackage
+    return Arrays.<ReactPackage>asList(
+            new RealmReactPackage(),
+            new RCTCameraPackage(),
+            new RNFSPackage()
+    );
+  }
+
+  @Override
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return getPackages();
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+  }
+}
